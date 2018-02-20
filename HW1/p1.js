5abf5916624b7ee16664ebe9452ef1a23b05e4b1
Leyaim Misael Jimenez Elizondo <ljimenez6@ucol.mx>
2018-02-20 08:25:43 -0600

    update 1
    function practica

--- HW1/p1.js
+++ HW1/p1.js
@@ -11,3 +11,32 @@ function suma1(x1,x2){
 
 //var suma = n1+n2;
 console.log(suma1);
+
+function greet(){
+console.log('hi');
+}
+greet();
+
+
+//functions are fist class
+function logGreeting(fn){
+ fn();
+}
+logGreeting(greet);
+//function expresion
+let greetMe = function(){
+  console.log('hi tony!');
+}
+greetMe();
+
+// its firsclass
+
+logGreeting(greetMe);
+
+//use function expresion to create 
+
+logGreeting(() =>{
+console.log('hello tony!');
+
+});
+
